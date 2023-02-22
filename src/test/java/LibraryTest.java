b6d7524c67c1628c07bd3744a3a91e03695c3e83
Fraser <fraser.russell1991@gmail.com>
2023-02-22 09:42:16 +0000

    code tidy

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -49,25 +49,3 @@ public class LibraryTest {
 
 }
 
-//import org.junit.Before;
-//        import org.junit.Test;
-//
-//        import static org.junit.Assert.assertEquals;
-//
-//public class RiverTest {
-//
-//        private River river;
-//        private Salmon salmon;
-//
-//        @Before
-//        public void before(){
-//                river = new River();
-//                salmon = new Salmon();
-//        }
-//
-//        @Test
-//        public void canAddSalmon(){
-//                river.addFish(salmon);
-//                assertEquals(1, river.fishCount());
-//        }
-
